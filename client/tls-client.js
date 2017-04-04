6ce437372836798a76df3c7cb5080e44df5bc729
tmulroy <thomasjohnmulroy@gmail.com>
2017-04-04 09:10:49 -0400

    add ability for bidirectional communication

--- client/tls-client.js
+++ client/tls-client.js
@@ -19,9 +19,9 @@ const options = {
 const socket = tls.connect(options, () => {
   console.log('\nclient connected',
               socket.authorized ? 'is authorized' : 'is unauthorized');
+  // redirect stdin to the socket (allows user to type in shell)
   process.stdin.pipe(socket);
   process.stdin.resume();
-  process.stdout
   console.log(`\nAddress: ${socket.address().address}`);
   console.log(`Port: ${socket.address().port}`);
   console.log(`Family: ${socket.address().family}\n`);
@@ -29,8 +29,9 @@ const socket = tls.connect(options, () => {
 
 socket.setEncoding('utf8');
 
+// need to fix, if client writes to socket, it outputs to client stdout
 socket.on('data', (data) => {
-  console.log(data);
+  console.log(`Server: ${data}`);
 });
 
 socket.on('end', () => {
--- server/tls-server.js
+++ server/tls-server.js
@@ -8,27 +8,28 @@ const options = {
     key: fs.readFileSync('./certificate/server.key'),
     cert: fs.readFileSync('./certificate/server.crt'),
     ca: fs.readFileSync('../certificate_authority/ca.crt'),
-    requestCert: true, // ask for a client cert
-    //rejectUnauthorized: false, // act on unauthorized clients at the app level
+    requestCert: true,
+    rejectUnauthorized: false
 };
 
 let server = tls.createServer(options, (socket) => {
   socket.write('Server: welcome!\n');
   socket.setEncoding('utf8');
   socket.pipe(socket);
+  // redirect stdin to the socket (allows user to type in shell)
+  process.stdin.pipe(socket);
   console.log(`Cipher Suite: ${socket.getCipher().name}`);
   console.log(`Protocol: ${socket.getProtocol()}`);
 });
 
 
-server.on('secureConnection', (c) => {
-	// c.authorized will be true if the client cert presented validates with our CA
-	console.log('secure connection; client authorized: ', c.authorized);
+server.on('secureConnection', (socket) => {
+	console.log('secure connection; client authorized: ', socket.authorized);
+  socket.on('data', (data) => {
+    console.log(`Client: ${data}`)
+  });
 });
 
-server.on('data', (data) => {
-  console.log(`Client: ${data}`)
-});
 
 server.listen(port, () => {
 	console.log(`Server listening on port ${port} \n`);
